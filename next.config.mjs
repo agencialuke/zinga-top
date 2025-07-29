32dd4243ad5ba6c4e053c4de7a83894197f2e1c2
agencialuke <agencialuke@gmail.com>
2025-07-29 14:42:52 -0300

    Corrigido next.config.mjs com configuração de domínio de imagem

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
-HEAD
-const nextConfig = {};
-
 const nextConfig = {
   images: {
     domains: ['vitrines-zinga-top.s3.us-east-2.amazonaws.com'],
   },
-};
-801af21 (deploy para vercel)
+  output: 'standalone' // Opcional, adicione se estiver usando deploy com Docker/Vercel standalone
+}
 
-export default nextConfig;
+export default nextConfig
